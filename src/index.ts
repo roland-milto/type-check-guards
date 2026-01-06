78d2ca89d026b2887f46adcccfd8b065dece3e83
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-01-06 14:15:21 +0100

    Added new functions isPrimitive/arePrimitives and isFloats/areFloats. Removed examples.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {areBooleans, isBoolean} from "./checks/boolean.js";
 import {areBuffers, isBuffer} from "./checks/buffer.js";
 import {areDates, isDate} from "./checks/date.js";
 import {areFinite, isFinite} from "./checks/finite.js";
+import {areFloats, isFloat} from "./checks/float.js";
 import {areFunctions, isFunction} from "./checks/function.js";
 import {areIntegers, isInteger} from "./checks/integer.js";
 import {areNull, isNull} from "./checks/null.js";
@@ -16,6 +17,7 @@ import {areNumbers, isNumber} from "./checks/number.js";
 import {areObjects, isObject} from "./checks/object.js";
 import {areOfType, isOfType} from "./checks/ofType.js";
 import {arePlainObjects, isPlainObject} from "./checks/plainObject.js";
+import {arePrimitives, isPrimitive} from "./checks/primitive.js";
 import {arePromises, isPromise} from "./checks/promise.js";
 import {areRegExes, isRegEx} from "./checks/regEx.js";
 import {areStreams, isStream} from "./checks/stream.js";
@@ -36,6 +38,7 @@ import {getTypeOf} from "./utils/getTypeOf.js";
  * * `isBuffer`: Returns a boolean whether the value is a buffer.
  * * `isDate`: Returns a boolean whether the value is a date.
  * * `isFinite`: Returns a boolean whether the value is finite.
+ * * `isFloat`: Returns a boolean whether the value is a float.
  * * `isFunction`: Returns a boolean whether the value is a function.
  * * `isInteger`: Returns a boolean whether the value is an integer.
  * * `isNull`: Returns a boolean whether the value is null.
@@ -44,6 +47,7 @@ import {getTypeOf} from "./utils/getTypeOf.js";
  * * `isObject`: Returns a boolean whether the value is an object.
  * * `isOfType`: Validates if the value matches a specified data type.
  * * `isPlainObject`: Returns a boolean whether the value is a plain object.
+ * * `isPrimitive`: Returns a boolean whether the value is a primitive.
  * * `isPromise`: Returns a boolean whether the value is a promise.
  * * `isRegEx`: Returns a boolean whether the value is a regular expression.
  * * `isStream`: Returns a boolean whether the value is a stream.
@@ -59,6 +63,7 @@ import {getTypeOf} from "./utils/getTypeOf.js";
  * * `areBuffers`: Returns a boolean whether all elements in the array are buffers.
  * * `areDates`: Returns a boolean whether all elements in the array are dates.
  * * `areFinite`: Returns a boolean whether all elements in the array are finite.
+ * * `areFloats`: Returns a boolean whether all elements in the array are floats.
  * * `areFunctions`: Returns a boolean whether all elements in the array are functions.
  * * `areIntegers`: Returns a boolean whether all elements in the array are integers.
  * * `areNull`: Returns a boolean whether all elements in the array are null.
@@ -67,6 +72,7 @@ import {getTypeOf} from "./utils/getTypeOf.js";
  * * `areObjects`: Returns a boolean whether all elements in the array are objects.
  * * `areOfType`: Returns a boolean whether all elements in the array match the specified data type.
  * * `arePlainObjects`: Returns a boolean whether all elements in the array are plain objects.
+ * * `arePrimitives`: Returns a boolean whether all elements in the array are primitives.
  * * `arePromises`: Returns a boolean whether all elements in the array are promises.
  * * `areRegExes`: Returns a boolean whether all elements in the array are regular expressions.
  * * `areStreams`: Returns a boolean whether all elements in the array are streams.
@@ -76,251 +82,55 @@ import {getTypeOf} from "./utils/getTypeOf.js";
  * * `areValidDates`: Returns a boolean whether all elements in the array are valid dates.
  *
  * @author  Roland Milto (https://roland.milto.de/)
- * @version 2025-12-22
+ * @version 2026-01-06
  */
 export const type: Readonly<TypeMethods> = Object.freeze(
 {
-  /**
-   * @param {unknown} value - The parameter to convert to a string.
-   *
-   * @example
-   * // Returns “string”:
-   * type.getTypeOf("Roland Milto");
-   */
   getTypeOf,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isArray([1, 2, 3]);
-   * type.areArrays([1, 2, 3], [true, false], ["Roland", "Milto"]);
-   *
-   * // Returns "false":
-   * type.isArray(5);
-   */
   isArray,
   areArrays,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isBigInt(123n);
-   *
-   * // Returns "false":
-   * type.isBigInt(123);
-   */
   isBigInt,
   areBigInts,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isBoolean(true);
-   * type.isBoolean(false);
-   *
-   * // Returns "false":
-   * type.isBoolean(0);
-   * type.isBoolean("true");
-   */
   isBoolean,
   areBooleans,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isBuffer(Buffer.from("abc"));
-   *
-   * // Returns "false":
-   * type.isBuffer("abc");
-   */
   isBuffer,
   areBuffers,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isDate(new Date());
-   *
-   * // Returns "false":
-   * type.isDate("2025-12-22");
-   */
   isDate,
   areDates,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isFinite(123);
-   *
-   * // Returns "false":
-   * type.isFinite(Infinity);
-   */
   isFinite,
   areFinite,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isFunction(() => {});
-   *
-   * // Returns "false":
-   * type.isFunction(123);
-   */
+  isFloat,
+  areFloats,
   isFunction,
   areFunctions,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isInteger(123);
-   *
-   * // Returns "false":
-   * type.isInteger(123.45);
-   */
   isInteger,
   areIntegers,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isNull(null);
-   *
-   * // Returns "false":
-   * type.isNull(undefined);
-   */
   isNull,
   areNull,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isNullOrUndefined(null);
-   * type.isNullOrUndefined(undefined);
-   *
-   * // Returns "false":
-   * type.isNullOrUndefined("Roland Milto");
-   */
   isNullOrUndefined,
   areNullOrUndefined,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isNumber(2);
-   *
-   * // Returns “false”:
-   * type.isNumber("2");
-   */
   isNumber,
   areNumbers,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isObject({ name: "Roland Milto" });
-   *
-   * // Returns “false”:
-   * type.isObject(5);
-   */
   isObject,
   areObjects,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isOfType("hello", "string");
-   *
-   * // Returns "false":
-   * type.isOfType(123, "string");
-   */
   isOfType,
   areOfType,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isPlainObject({ a: 1 });
-   *
-   * // Returns "false":
-   * type.isPlainObject(new Date());
-   */
   isPlainObject,
   arePlainObjects,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isPromise(Promise.resolve(123));
-   *
-   * // Returns "false":
-   * type.isPromise(123);
-   */
+  isPrimitive,
+  arePrimitives,
   isPromise,
   arePromises,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isRegEx(/abc/);
-   *
-   * // Returns "false":
-   * type.isRegEx("abc");
-   */
   isRegEx,
   areRegExes,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isStream(fs.createReadStream("file.txt"));
-   *
-   * // Returns "false":
-   * type.isStream({});
-   */
   isStream,
   areStreams,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isString("Roland Milto");
-   *
-   * // Returns “false”:
-   * type.isString(5);
-   */
   isString,
   areStrings,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isSymbol(Symbol("id"));
-   *
-   * // Returns "false":
-   * type.isSymbol("id");
-   */
   isSymbol,
   areSymbols,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isUndefined(undefined);
-   *
-   * // Returns "false":
-   * type.isUndefined(null);
-   */
   isUndefined,
   areUndefined,
-
-  /**
-   * @example
-   * // Returns "true":
-   * type.isValidDate(new Date());
-   *
-   * // Returns "false":
-   * type.isValidDate(new Date("invalid"));
-   */
   isValidDate,
   areValidDates
 });
@@ -333,6 +143,7 @@ export {areBooleans, isBoolean} from './checks/boolean.js';
 export {areBuffers, isBuffer} from './checks/buffer.js';
 export {areDates, isDate} from './checks/date.js';
 export {areFinite, isFinite} from './checks/finite.js';
+export {areFloats, isFloat} from './checks/float.js';
 export {areFunctions, isFunction} from './checks/function.js';
 export {areIntegers, isInteger} from './checks/integer.js';
 export {areNull, isNull} from './checks/null.js';
@@ -340,6 +151,7 @@ export {areNullOrUndefined, isNullOrUndefined} from './checks/nullOrUndefined.js
 export {areNumbers, isNumber} from './checks/number.js';
 export {areObjects, isObject} from './checks/object.js';
 export {areOfType, isOfType} from './checks/ofType.js';
+export {arePrimitives, isPrimitive} from './checks/primitive.js';
 export {arePromises, isPromise} from './checks/promise.js';
 export {arePlainObjects, isPlainObject} from './checks/plainObject.js';
 export {areRegExes, isRegEx} from './checks/regEx.js';
