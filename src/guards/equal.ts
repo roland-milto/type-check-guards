10cce1cff27875092796cc6b8b909ab0571e4693
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-01-13 12:35:27 +0100

    Added function "areEqual"

--- src/guards/equal.ts
+++ src/guards/equal.ts
@@ -1,3 +1,6 @@
+// Import: Self-created functions.
+import {isFilledArray} from "./filledArray.js";
+
 /**
  * Determines whether the given value is strictly equal to the expected value.
  *
@@ -8,4 +11,30 @@
  */
 export function isEqual<T>(value: unknown, expected: T): value is T {
   return value === expected;
+}
+
+/**
+ * Determines whether all elements in the given array are strictly equal to the expected value.
+ *
+ * @author  Roland Milto (https://roland.milto.de/)
+ * @version 2026-01-13
+ *
+ * @param   {unknown} value       - The value to be checked (expected to be an array).
+ * @param   {unknown} expected    - The expected value for comparison.
+ *
+ * @returns {value is unknown[]}  - Returns `true` if the value is an array and all its elements
+ *                                are strictly equal to the expected value, otherwise `false`.
+ */
+export function areEqual<T>(value: unknown, expected: T): value is T[] {
+  if (!isFilledArray(value)) {
+    return false;
+  }
+
+  for (const item of value) {
+    if (!isEqual(item, expected)) {
+      return false;
+    }
+  }
+
+  return true;
 }
