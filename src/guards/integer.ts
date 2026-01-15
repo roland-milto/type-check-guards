03f38934109771045c769ecbad6f2c58ca1d82e7
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-01-15 15:47:50 +0100

    Changed internal check to isSafeInteger for integer validation

--- src/guards/integer.ts
+++ src/guards/integer.ts
@@ -2,14 +2,14 @@
 import {isFilledArray} from "./filledArray.js";
 
 /**
- * Checks if the given value is an integer.
+ * Checks if the given value is a safe integer (maximum 9.007.199.254.740.991).
  *
  * @author  Roland Milto (https://roland.milto.de/)
  * @version 2025-12-22
  *
  * @param   {unknown} value - The value to check.
  *
- * @returns {boolean}       - Returns `true` if the value is an integer, otherwise `false`.
+ * @returns {boolean}       - Returns `true` if the value is a safe integer, otherwise `false`.
  *
  *  @example
  *  isInteger(5) // true
@@ -17,18 +17,18 @@ import {isFilledArray} from "./filledArray.js";
  *  isInteger("5") // false
  */
 export function isInteger(value: unknown): value is number {
-  return typeof value === 'number' && Number.isInteger(value);
+  return typeof value === 'number' && Number.isSafeInteger(value);
 }
 
 /**
- * Checks if the provided value is an array of integers.
+ * Checks if the provided value is an array of safe integers.
  *
  * @author  Roland Milto (https://roland.milto.de/)
  * @version 2026-01-07
  *
  * @param   {unknown} array - The value to be checked.
  *
- * @returns {boolean}       - Returns `true` if the value is an array where all elements are integers, otherwise `false`.
+ * @returns {boolean}       - Returns `true` if the value is an array where all elements are safe integers, otherwise `false`.
  *
  * @example
  * isInteger([5, 1337]) // true
