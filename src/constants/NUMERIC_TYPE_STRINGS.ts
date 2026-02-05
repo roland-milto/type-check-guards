2c61bdd20fcf74bf62af36134d886708d82c5676
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-02-05 21:29:59 +0100

    Numeric types

---
+++ src/constants/NUMERIC_TYPE_STRINGS.ts
@@ -0,0 +1,30 @@
+/**
+ * A mapping representing different classifications of numerical data types.
+ *
+ * - "bigint": Represents a numeric type for arbitrarily large integers.
+ * - "binary" for binary numeral systems (base-2)
+ * - "decimal": Represents a numeric type for decimal numeral systems (base-10)
+ * - "float": Represents a numeric type for floating-point numbers.
+ * - "hexadecimal": Represents a numeric type for hexadecimal numeral systems (base-16)
+ * - "integer": Represents a numeric type for whole numbers.
+ * - "nan": Represents a numeric type for Not-a-Number (NaN) values.
+ * - "number": Represents a general numeric type that can include both integers and floating-point numbers.
+ * - "octal": Represents a numeric type for octal numeral systems (base-8)
+ *
+ * @author  Roland Milto (https://roland.milto.de/)
+ * @version 2026-01-29
+ */
+const NUMERIC_TYPE_STRINGS = [
+  "bigint",
+  "binary",
+  "decimal",
+  "float",
+  "hexadecimal",
+  "integer",
+  "nan",
+  "number",
+  "octal",
+] as const;
+
+// Export constant.
+export {NUMERIC_TYPE_STRINGS};
--- src/types/numericType.ts
+++ src/types/numericType.ts
@@ -1,33 +1,82 @@
+// Import: local variables.
+import {NUMERIC_TYPE_STRINGS} from "../constants/NUMERIC_TYPE_STRINGS.js";
+
 /**
- * A mapping representing different classifications of numerical data types.
+ * The `NumericTypeAsString` type represents a string from the `NUMERIC_TYPE_STRINGS` array.
  *
- * - "bigint": Represents a numeric type for arbitrarily large integers.
- * - "binary" for binary numeral systems (base-2)
- * - "decimal": Represents a numeric type for decimal numeral systems (base-10)
- * - "float": Represents a numeric type for floating-point numbers.
- * - "hexadecimal": Represents a numeric type for hexadecimal numeral systems (base-16)
- * - "integer": Represents a numeric type for whole numbers.
- * - "nan": Represents a numeric type for Not-a-Number (NaN) values.
- * - "number": Represents a general numeric type that can include both integers and floating-point numbers.
- * - "octal": Represents a numeric type for octal numeral systems (base-8)
+ * It allows for representing numeric types as strings in a type-safe manner derived from a predefined list.
  *
  * @author  Roland Milto (https://roland.milto.de/)
- * @version 2026-01-26
+ * @version 2026-01-29
+ *
+ * @example
+ * // true
+ * const example1: NumericTypeAsString = "integer";
+ *
+ * // true
+ * const example2: NumericTypeAsString = "float";
+ *
+ * // false (error; not part of NUMERIC_TYPE_STRINGS)
+ * const example3: NumericTypeAsString = "string";
  */
-type _NumericType =
-  {
-    bigint: number;
-    binary: number;
-    decimal: number;
-    float: number;
-    hexadecimal: number;
-    integer: number;
-    nan: number;
-    number: number;
-    octal: number;
-  }
+type NumericTypeAsString = (typeof NUMERIC_TYPE_STRINGS)[number];
+
+/**
+ * The `_NumericType` type defines a TypeScript record where keys are strings representing numeric types,
+ * `NumericTypeAsString`, and the associated values are numbers.
+ * This type ensures a structured way to map different string representations of numeric types to corresponding numeric values.
+ *
+ * The functionality is typically used to handle flexible number type representations in typing-safe environments.
+ *
+ * @author  Roland Milto (https://roland.milto.de/)
+ * @version 2026-01-29
+ */
+type _NumericType = Record<NumericTypeAsString, number>;
+
+/**
+ * The `NumericType` type represents a mapping from string representations of numeric types to their corresponding
+ * internal numeric type definitions (real types). It is providing type safety and clarity when handling numeric data types.
+ *
+ * @author  Roland Milto (https://roland.milto.de/)
+ * @version 2026-01-29
+ *
+ * @example
+ * // true
+ * const numType: NumericType = 45;
+ *
+ * // false (error; string given)
+ * const numType: NumericType = "integer";
+ */
+type NumericType = _NumericType[NumericTypeAsString];
+
+/**
+ * Represents a type mapping from a string representation of numeric types
+ * to their corresponding actual numeric types.
+ *
+ * @author  Roland Milto (https://roland.milto.de/)
+ * @version 2026-01-29
+ *
+ * @template K - The string representation of the numeric type.
+ *
+ * @example
+ * // NumericTypeOf<"float"> is number
+ *
+ * // true (successfully maps "integer" to a number type)
+ * const intType: NumericTypeOf<'int'> = 123;
+ *
+ * // true (successfully maps "float" to a number type)
+ * const floatType: NumericTypeOf<'float'> = 123.45;
+ *
+ * // false (GenericType does not map to a number type)
+ * const stringType: NumericTypeOf<'unknown'> = "string"; // Type error
+ *
+ * // false (input "currency" is not a known numeric type)
+ * const price: NumericTypeOf<'currency'> = 9.99; // Type error
+ *
+ * // true (representing the result of a math operation)
+ * const result: NumericTypeOf<'float'> = 3.14 * 2;
+ */
+type NumericTypeOf<K extends NumericTypeAsString> = _NumericType[K];
 
 // Export types.
-export type NumericTypeAsString = keyof _NumericType;
-export type NumericType = _NumericType[NumericTypeAsString];
-export type NumericTypeOf<K extends NumericTypeAsString> = _NumericType[K]; // NumericTypeOf<"float"> is number
+export type {NumericTypeAsString, NumericType, NumericTypeOf};
