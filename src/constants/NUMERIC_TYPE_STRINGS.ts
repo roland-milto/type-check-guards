e6c660d1e41d0ff09b0ad813e20e433ded07f7e7
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-02-14 10:15:29 +0100

    Added "numeric" as new numeric type

--- src/constants/NUMERIC_TYPE_STRINGS.ts
+++ src/constants/NUMERIC_TYPE_STRINGS.ts
@@ -9,10 +9,11 @@
  * - "integer": Represents a numeric type for whole numbers.
  * - "nan": Represents a numeric type for Not-a-Number (NaN) values.
  * - "number": Represents a general numeric type that can include both integers and floating-point numbers.
+ * - "numeric": Represents a general numeric type that can include any numeric type, excluding NaN values.
  * - "octal": Represents a numeric type for octal numeral systems (base-8)
  *
  * @author  Roland Milto (https://roland.milto.de/)
- * @version 2026-01-29
+ * @version 2026-02-05
  */
 const NUMERIC_TYPE_STRINGS = [
   "bigint",
@@ -23,6 +24,7 @@ const NUMERIC_TYPE_STRINGS = [
   "integer",
   "nan",
   "number",
+  "numeric",
   "octal",
 ] as const;
 
